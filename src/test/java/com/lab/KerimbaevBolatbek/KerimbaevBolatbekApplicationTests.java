0f345faef447a8d94934beb68735a2ec32d718af
bekbolatbekkhan <kerimbaevbolatbek06@gmail.com>
2026-05-20 09:52:11 +0600

    test: add basic test structure

---
+++ src/test/java/com/lab/KerimbaevBolatbek/KerimbaevBolatbekApplicationTests.java
@@ -0,0 +1,13 @@
+package com.lab.KerimbaevBolatbek;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class KerimbaevBolatbekApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
