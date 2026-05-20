6f037578c1b0bf3945b72c36df053fae114853b2
bekbolatbekkhan <kerimbaevbolatbek06@gmail.com>
2026-05-20 09:51:07 +0600

    feat: add main Spring Boot Application class

---
+++ src/main/java/com/lab/KerimbaevBolatbek/KerimbaevBolatbekApplication.java
@@ -0,0 +1,13 @@
+package com.lab.KerimbaevBolatbek;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class KerimbaevBolatbekApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(KerimbaevBolatbekApplication.class, args);
+    }
+
+}
